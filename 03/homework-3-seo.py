51dac419f7d45e6743d5510bea5e367a086218c0
June Seo <jeowseo@gmail.com>
2016-06-02 10:07:09 -0400

    laaaa

--- 03/homework-3-seo.py
+++ 03/homework-3-seo.py
@@ -3,16 +3,34 @@
 #Homework3
 
 #LISTS
+
 #1
 countries = ['South Korea', 'North Korea', 'Australia', 'Greece', 'Costa Rica', 'Mexico', 'Japan']
 
 #2
 for lala in countries:
-    print lala
+    print (lala)
 
 #3
 countries = ['South Korea', 'North Korea', 'Australia', 'Greece', 'Costa Rica', 'Mexico', 'Japan']
-print ('SORTED')
-print (sorted(countries))
+print('-----------SORTED----------')
+print(countries.sort())
 
 #4
+print(countries[0])
+
+#5
+print(countries[len(countries)-1])
+
+#6
+print ('-----DELETE ONE COUNTRY-----')
+countries.remove('South Korea')
+print (countries)
+
+#7
+for lalala in countries:
+    print (lalala)
+
+#DICTIONARIES
+#1
+tree = {}
